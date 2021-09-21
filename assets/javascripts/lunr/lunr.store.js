b18a9c45ef67a5b63fc9d4558390bbf812aa1c36
Ken Wakita <wakita@is.titech.ac.jp>
2021-09-21 22:15:53 +0900

    publish

--- assets/javascripts/lunr/lunr.store.js
+++ assets/javascripts/lunr/lunr.store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "リンク",
+        "excerpt":"リンク (Ken Wakita, 2021-09-21) リンクの貼り方について悩んだ。いくつかの設定と Jekyll のバージョンが絡んだ問題だったようだ。   baseurl と url の設定：なにかの例を見て url に GitHub の URL を設定していたが、それを空にした。      baseurl: \"/classes\"  url: \"\"                   Jekyll V4.0 以降ではリンクの設定が簡素化され、site.baseurl の参照が不要になったとドキュメントに書かれている。ということは、それより古いバージョンの Jekyll を利用している場合はこれを明示すべきなのだろう。       ここにある[各クラスの第4クォーターの内容説明](/classes/y21/cs2/course.html)を読んで    なぜ、手元の Jekyll のバージョンが 3.9.0 と古いのかは不明。   ","categories": [],
+        "tags": [],
+        "url": "/classes/2021/09/21/link.html"
+      },{
         "title": "コンピュータサイエンス第一",
         "excerpt":"LAS.I121 2021年度 コンピュータサイエンス第一   担当教員  脇田　建  授業の形式  講義 / 演習 - 1単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": ["class"],
         "tags": [],

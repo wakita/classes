ede2f381502020f6bcace683ee4a33dbce4ad11c
Ken Wakita <wakita@is.titech.ac.jp>
2021-09-29 21:09:59 +0900

    publish

--- assets/javascripts/lunr/lunr.store.js
+++ assets/javascripts/lunr/lunr.store.js
@@ -5,32 +5,32 @@ var store = [{
         "url": "/classes/2021/09/21/link.html"
       },{
         "title": "コンピュータサイエンス第一",
-        "excerpt":"LAS.I121 2021年度 コンピュータサイエンス第一   担当教員  脇田　建  授業の形式  講義 / 演習 - 1単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": ["class"],
+        "excerpt":"LAS.I121 2021年度 3Q コンピュータサイエンス第一   担当教員  脇田　建  授業の形式  講義 / 演習 - 1単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": [],
         "tags": [],
         "url": "/classes/y21/cs1.html"
       },{
         "title": "コンピュータサイエンス第二",
-        "excerpt":"LAS.I122 2021年度 コンピュータサイエンス第二   担当教員  脇田　建  授業の形式  講義 / 演習 - 1単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": ["class"],
+        "excerpt":"LAS.I122 2021年度 4Q コンピュータサイエンス第二   担当教員  脇田　建  授業の形式  講義 / 演習 - 1単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": [],
         "tags": [],
         "url": "/classes/y21/cs2.html"
       },{
         "title": "プログラミング第一",
-        "excerpt":"MCS.T224 2021年度 プログラミング第一   担当教員  脇田 建・叢 悠悠  授業の形式  講義 / 演習 - 3単位; W833  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": ["class"],
+        "excerpt":"MCS.T224 2021年度 3Q プログラミング第一   担当教員  脇田 建・叢 悠悠  授業の形式  講義 / 演習 - 3単位; 月曜 – W641，火曜・金曜 W631  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク  Day 1","categories": [],
         "tags": [],
         "url": "/classes/y21/prg1.html"
       },{
         "title": "研究プロジェクト",
-        "excerpt":"MCS.Z381-02 2021年度 研究プロジェクト   担当教員  脇田　建  授業の形式  実習 - 2単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク      第一ラウンド         第二ラウンド         第三ラウンド   ","categories": ["class"],
+        "excerpt":"MCS.Z381-02 2021年度 34Q 研究プロジェクト   担当教員  脇田　建  授業の形式  実習 - 2単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク      第一ラウンド         第二ラウンド         第三ラウンド   ","categories": [],
         "tags": [],
         "url": "/classes/y21/prj.html"
       },{
         "title": "情報可視化",
-        "excerpt":"MCS.T412 2021年度 情報可視化   担当教員  脇田　建  授業の形式  講義 - 2単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": ["class"],
+        "excerpt":"MCS.T412 2021年度 4Q 情報可視化   担当教員  脇田　建  授業の形式  講義 - 2単位; Zoom  使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": [],
         "tags": [],
         "url": "/classes/y21/vis.html"
       },{
         "title": "はじめてのPython",
-        "excerpt":"# 2022年度 はじめてのPython   担当教員  脇田　建  授業の形式            単位;        使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": ["class"],
+        "excerpt":"# 2022年度 2Q はじめてのPython   担当教員  脇田　建  授業の形式            単位;        使用言語  日本語  T2Schola  T2Scholaへのリンク  シラバス  シラバスへのリンク","categories": [],
         "tags": [],
         "url": "/classes/y22/py.html"
       }]
